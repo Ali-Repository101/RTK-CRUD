0983237358ab14e876970f068533d19ff16a43df
ali <ali@gmail.com>
2022-12-30 10:58:17 +0530

    Upload image in Judit Editor

--- src/Components/Form/CsJoditEditor.js
+++ src/Components/Form/CsJoditEditor.js
@@ -2,16 +2,21 @@ import React, { useState, useRef, useMemo } from 'react';
 import JoditEditor from 'jodit-react';
 
 const CsJoditEditor = ( props ) => {
-    // console.log("inside props", props)
-    // console.log("inside props", props.onBlur)
+	const config = {
+        uploader: {
+              insertImageAsBase64URI: false,
+              url: 'http://your-upload-file-server-host/test-upload-file.php',
+        },
+}
 	const editor = useRef(null);
 	// const [content, setContent] = useState('');
 
-	return (
+	return (	
 		<JoditEditor
 			ref={editor}
 			value={props.content}
 			onBlur={(e) => props.onBlur(e)} 
+			config={config}
 			onChange={(newContent) => {}}
 		/>
 	);
