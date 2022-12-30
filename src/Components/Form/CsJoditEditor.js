f74fe08a612af763196711e57d3c854be6442297
HarnoorAtcode <118904859+HarnoorAtcode@users.noreply.github.com>
2022-12-30 16:41:20 +0530

    validation fixes

--- src/Components/Form/CsJoditEditor.js
+++ src/Components/Form/CsJoditEditor.js
@@ -2,8 +2,12 @@ import React, { useState, useRef, useMemo } from 'react';
 import JoditEditor from 'jodit-react';
 
 const CsJoditEditor = ( props ) => {
-    // console.log("inside props", props)
-    // console.log("inside props", props.onBlur)
+	const config = {
+        uploader: {
+              insertImageAsBase64URI: true,
+            //   url: '',
+        },
+}
 	const editor = useRef(null);
 	// const [content, setContent] = useState('');
 
@@ -11,8 +15,10 @@ const CsJoditEditor = ( props ) => {
 		<JoditEditor
 			ref={editor}
 			value={props.content}
+			config={config}
 			onBlur={(e) => props.onBlur(e)} 
 			onChange={(newContent) => {}}
+			className="mt-5"
 		/>
 	);
 };
